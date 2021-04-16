7b2caaffd46ac60a5af3c20bd442d3382a638318
GitHub Action <action@github.com>
2021-04-16 07:22:08 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/kclay/intelljclickupplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kclay.intelljclickupplugin.services
+
+import com.github.kclay.intelljclickupplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
